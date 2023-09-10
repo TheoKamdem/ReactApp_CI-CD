4e03f7ca8ae3cf20ba1f5f6cc7e79eb19ed4f841
Theophile KAMDEM <95441284+blackmanba7@users.noreply.github.com>
2023-09-10 16:27:30 +0100

    Add   file yml  for  github action

--- src/App.js
+++ src/App.js
@@ -35,15 +35,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Team />} />
               <Route path="/dasboard" element={<Dashboard />} />
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
-              {/* <Route path="/form" element={<Form />} /> */}
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/contacts" element={<Contacts />} />
+             
             </Routes>
           </main>
         </div>
