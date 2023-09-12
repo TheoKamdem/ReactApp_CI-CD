52ca3af27a14219bb3811916a612c3ef55c6d1c6
Theophile KAMDEM <95441284+TheoKamdem@users.noreply.github.com>
2023-09-12 20:53:56 +0100

    fix  class test  form  app.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Importez BrowserRouter
+
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <Router> {/* Enveloppez votre composant App avec Router */}
+      <App />
+    </Router>
+  );
 });
